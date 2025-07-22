af62cb99e70e823b98acb40dba922d8479d2d870
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-22 16:17:49 +0200

    Metadata title object

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { type Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    absolute: 'Blog Page',
+  },
+};
+
 const BlogPage = () => {
   return (
     <div>BlogPage</div>
