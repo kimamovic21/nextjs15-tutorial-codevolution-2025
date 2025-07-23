c6e0a26ecf330f7f4ab693f402156e67bff9070f
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-23 15:50:49 +0200

    Recovering from errors

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,9 +1,33 @@
 'use client';
 
-const ErrorBoundary = ({ error }: { error: Error }) => {
+import { startTransition } from 'react';
+import { useRouter } from 'next/navigation';
+
+const ErrorBoundary = ({
+  error,
+  reset
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  const router = useRouter();
+
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
     <div>
-      {error?.message}
+      <p>{error?.message}</p>
+      <button
+        onClick={() => reload()}
+        className='bg-blue-500 px-2 py-1 rounded-sm cursor-pointer'
+      >
+        Try again
+      </button>
     </div>
   );
 };
