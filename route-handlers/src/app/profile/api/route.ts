7025cbd7bb8454804fb0c9acece9ef8168bcdf2b
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-24 17:23:33 +0200

    Cookies in route handlers

--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,16 +1,24 @@
 import { type NextRequest } from 'next/server';
-import { headers } from 'next/headers';
+import { headers, cookies } from 'next/headers';
 
 export async function GET(request: NextRequest) {
-  // const requestHeaders = new Headers(request.headers);
-  // console.log(requestHeaders.get('Authorization'));
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get('Authorization'));
 
   const headerList = await headers();
   console.log(headerList.get('Authorization'));
 
+  const theme = request.cookies.get('theme');
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set('resultsPerPage', '20');
+  console.log(cookieStore.get('resultsPerPage'));
+
   return new Response('<h1>Profile api data</h2>', {
     headers: {
       'Content-Type': 'text/html',
+      'Set-Cookie': 'theme=dark'
     },
   });
 };
