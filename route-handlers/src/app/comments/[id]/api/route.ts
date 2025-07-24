b0a7e5c8b04bdbaee032b2f00ce9c56d2b8dc927
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-24 15:36:31 +0200

    Handling PATCH Requests

--- route-handlers/src/app/comments/[id]/api/route.ts
+++ route-handlers/src/app/comments/[id]/api/route.ts
@@ -10,3 +10,20 @@ export async function GET(
 
   return Response.json(comment);
 };
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  const body = await request.json();
+
+  const { text } = body;
+
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
+};
