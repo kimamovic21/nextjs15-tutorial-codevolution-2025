3408405dca111223739d8fdf21fe70ac8e59e886
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-28 16:35:46 +0200

    Fetching from a database

---
+++ data-fetching/src/app/products-db/page.tsx
@@ -0,0 +1,35 @@
+import { getProducts } from '@/prisma-db';
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string | null;
+};
+
+const ProductsDBPage = async () => {
+  const products: Product[] = await getProducts();
+
+  return (
+    <ul className='space-y-4 p-4'>
+      {products?.map((product) => (
+        <li
+          key={product.id}
+          className='p-4 bg-white shadow-md rounded-lg text-gray-700'
+        >
+          <h2 className='text-xl font-semibold'>
+            {product.title}
+          </h2>
+          <p>
+            {product.description}
+          </p>
+          <p className='text-lg font-medium'>
+            ${product.price}
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ProductsDBPage;
