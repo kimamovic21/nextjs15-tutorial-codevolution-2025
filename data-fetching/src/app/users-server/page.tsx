6d72c303d81986c1a1f62ff07c89081b87970e05
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-28 14:37:01 +0200

    Loading and error states

--- data-fetching/src/app/users-server/page.tsx
+++ data-fetching/src/app/users-server/page.tsx
@@ -7,6 +7,9 @@ type User = {
 };
 
 const UsersServerPage = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  // const response = await fetch('https://jsonplaceholder.typicode.com/user');
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const users: User[] = await response.json();
 
