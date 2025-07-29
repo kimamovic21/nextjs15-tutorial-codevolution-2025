df34c7825b1d0bded68e8d7943615e69204c10fb
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-29 17:34:47 +0200

    Profile settings

--- auth-demo/src/components/navigation.tsx
+++ auth-demo/src/components/navigation.tsx
@@ -1,7 +1,12 @@
 import {
   SignInButton,
   SignOutButton,
+  SignUpButton,
+  UserButton,
+  SignedIn,
+  SignedOut,
 } from '@clerk/nextjs';
+import Link from 'next/link';
 
 const Navigation = () => {
   return (
@@ -9,14 +14,31 @@ const Navigation = () => {
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex justify-between h-16 items-center'>
           <div className='flex-shrink-0'>
-            <h1 className='text-xl font-semibold text-[var(--foreground)]'>
-              Auth Demo
-            </h1>
+            <Link href={'/'}>
+              <h2 className='text-xl font-semibold text-[var(--foreground)]'>
+                Auth Demo
+              </h2>
+            </Link>
           </div>
 
           <div className='flex items-center gap-4'>
-            <SignInButton mode='modal' />
-            <SignOutButton />
+            <SignedOut>
+              <SignInButton>
+                <button className='px-2 py-1 text-sm border border-neutral-300 dark:border-neutral-600 dark:text-neutral-200 dark:hover:bg-neutral-700'>
+                  Sign In
+                </button>
+              </SignInButton>
+              <SignUpButton>
+                <button className='px-2 py-1 text-sm border border-neutral-300 dark:border-neutral-600 dark:text-neutral-200 dark:hover:bg-neutral-700'>
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <Link href='/user-profile'>Profile</Link>
+              <SignOutButton />
+              <UserButton />
+            </SignedIn>
           </div>
         </div>
       </div>
